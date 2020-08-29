cebde0e6c8bd6410b81d0eeeb6cb72d7d546443a
JennH <marlowmayfield@hotmail.com>
2020-08-29 16:14:57 -0700

    made changes

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
   <main>
     <!--main menu item circle-->
     <div class="circular circleW">
-      <!--<h1 class="menu-title">Menu</h1>-->
+      <h1 class="menu-title">Menu</h1>
        <div class="text">
        <div class="horse-nav">
          <nuxt-link to="/horsetwo" class="horse-list">About</nuxt-link>
