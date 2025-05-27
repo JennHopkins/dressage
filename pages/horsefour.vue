356e9670797cca5ae9ab9e122847e21287505113
JennHopkins <56420133+JennHopkins@users.noreply.github.com>
2025-05-26 17:34:58 -0700

    Update horsefour.vue setting p text on this page as white

--- pages/horsefour.vue
+++ pages/horsefour.vue
@@ -3,6 +3,11 @@
 <section class="container">
   <head>
      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" integrity="sha384-KA6wR/X5RY4zFAHpv/CnoG2UW1uogYfdnP67Uv7eULvTveboZJg0qUpmJZb5VqzN" crossorigin="anonymous">
+ <style>
+      p{
+        color:white;
+      }
+   </style>
   </head>
     <a href="top"></a>
   <h1 class="title">Meet Our Trainers</h1>
